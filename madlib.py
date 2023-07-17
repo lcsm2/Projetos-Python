ba6d39627efae3e8edfb95329b8019e230addc08
lcsm2 <111532297+lcsm2@users.noreply.github.com>
2023-07-17 09:28:05 -0400

    Add files via upload

---
+++ madlib.py
@@ -0,0 +1,11 @@
+#PROJETO MAD LIBS
+nom = input("Qual é o seu nome? ")
+ida = input("Qual é a sua idade? ")
+cur = input("Qual é o seu curso da faculdade? ")
+per = input("Qual é o seu período na faculdade? ")
+est = input("O que você está estudando? ")
+
+madlib = f"Meu nome é {nom}, tenho {ida} anos de idade, \
+meu curso na faculdade é {cur} e estou no {per} período e estou estudando {est}."
+
+print(madlib)
