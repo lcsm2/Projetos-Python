709f09513b4256297d68041ef7e122f51cf3d85d
lcsm2 <111532297+lcsm2@users.noreply.github.com>
2023-07-18 09:54:42 -0400

    Update adivinhe.py

--- adivinhe.py
+++ adivinhe.py
@@ -2,11 +2,32 @@ import random
 print("Seja bem-vindo ao projeto de Adivinhação de números!")
 
 numero = input("Digite o número máximo do desafio: ")
-if numero.isdigit():
-    numero = int(numero)
+
+if numero.isdigit():     #Aqui verificamos se o número é realmente um número e transformamos ele em inteiro(int),
+    numero = int(numero) #pois quando ele é escrito no input ele está como string!
 else:
-    print("Erro: o valor que foi informado não é numérico. \
- Por favor execute o código novamente e coloque um valor numérico")
-quit()
+    print("Erro. O valor anterior informado não é um número, \
+    por favor coloque um valor correto.")
+    quit()
 
 numero_aleatorio = random.randint(0, numero)
+
+tentativas = 1
+
+while True:
+    pergunta = input("Advinhe o número: ")
+    if pergunta.isdigit():
+        pergunta = int(pergunta)
+    else:
+        print("Por favor coloque um valor numérico.")
+        continue
+
+    if  pergunta == numero_aleatorio:
+        print(f"Você acertou o número com um total de {tentativas}")   
+        break
+    elif pergunta > numero_aleatorio:
+        print("O número randômico é menor que isso...")
+        tentativas = tentativas + 1
+    else:
+        print("O número randômico é maior que isso...")
+        tentativas = tentativas + 1
